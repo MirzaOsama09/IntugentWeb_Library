2bd94ebcbacd2dfc63c32ac48d2fcb6d88e283e4
SheharyarXDツ <1510sheharyarxd@gmail.com>
2024-06-17 17:28:43 +0500

    UI In Progress

---
+++ IntugentWebApp/IntugentWebApp/Pages/Mfg_Group/DimStability.cshtml.cs
@@ -0,0 +1,11 @@
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace IntugentWebApp.Pages.Mfg_Group
+{
+    public class DimensionStabilityModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
---
+++ IntugentWebApp/IntugentWebApp/Utilities/ObjectsService.cs
@@ -0,0 +1,17 @@
+﻿using IntugentClassLbrary.Classes;
+using IntugentClassLibrary.Pages.Mfg;
+
+namespace IntugentWebApp.Utilities
+{
+    public class ObjectsService
+    {
+        public CDefualts CDefualts { get; set; }
+        public CLists CLists { get; set; }
+        public Cbfile Cbfile { get; set; }
+
+        public MfgHome MfgHome { get; set; }
+        public MfgInProcess? MfgInProcess { get; set; }
+        public MfgFinishedGoods? MfgFinishedGoods { get; set; }
+        public bool gInProcessDoneIsChecked { get; set; }
+    }
+}
